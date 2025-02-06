2c12b10e320239eb1141b7b726ae125dfb5803b4
John <johnroy@tomlinson.me>
2025-02-06 13:32:33 -0600

    frame drawer

---
+++ frames.js
@@ -0,0 +1,322 @@
+const frames = [
+    {
+        "id": "8053672909258",
+        "name": "OS6012",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS6012-BLK+(2024).jpg"
+      },
+      {
+        "id": "0888392486523",
+        "name": "OS6032",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS6032-BLK+(2024).jpg"
+      },
+      {
+        "id": "8056597233958",
+        "name": "OS6032",
+        "color": "Gold",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS6032-GLD+(2024).jpg"
+      },
+      {
+        "id": "8053672909258",
+        "name": "OS6038",
+        "color": "Navy",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS6038-NAV+(2024).jpg"
+      },
+      {
+        "id": "0888392486523",
+        "name": "OS6038",
+        "color": "Tortoise",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS6038-TOR+(2024).jpg"
+      },
+      {
+        "id": "8056597233958",
+        "name": "OS6062",
+        "color": "Purple",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS6062-PRP+(2024).jpg"
+      },
+      {
+        "id": "8053672909258",
+        "name": "OS6062",
+        "color": "Tortoise",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS6062-TOR+(2024).jpg"
+      },
+      {
+        "id": "0888392486523",
+        "name": "OS6063",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS6063-BLK+(2024).jpg"
+      },
+      {
+        "id": "8056597233958",
+        "name": "OS6063",
+        "color": "Purple",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS6063-PRP+(2024).jpg"
+      },
+      {
+        "id": "8053672909258",
+        "name": "OS6066",
+        "color": "Crystal",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS6066-CRY+(2024).jpg"
+      },
+      {
+        "id": "0888392486523",
+        "name": "OS6071",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS6071-BLK+(2024).jpg"
+      },
+      {
+        "id": "8056597233958",
+        "name": "OS6089",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS6089-BLK+(2024).jpg"
+      },
+      {
+        "id": "8053672909258",
+        "name": "OS6089",
+        "color": "Red",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS6089-RED+(2024).jpg"
+      },
+      {
+        "id": "0888392486523",
+        "name": "OS7009",
+        "color": "Gunmetal",
+        "type": "METAL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7009-GUN+(2024).jpg"
+      },
+      {
+        "id": "8056597233958",
+        "name": "OS7012",
+        "color": "Gunmetal",
+        "type": "METAL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7012-GUN+(2024).jpg"
+      },
+      {
+        "id": "8053672909258",
+        "name": "OS7017",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7017-BLK+(2024).JPG"
+      },
+      {
+        "id": "0888392486523",
+        "name": "OS7017",
+        "color": "Crystal",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7017-CRY+(2024).jpg"
+      },
+      {
+        "id": "8056597233958",
+        "name": "OS7044",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7044-BLK+(2024).jpg"
+      },
+      {
+        "id": "8053672909258",
+        "name": "OS7056",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7056-BLK+(2024).jpg"
+      },
+      {
+        "id": "0888392486523",
+        "name": "OS7057",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7057-BLK+(2024).jpg"
+      },
+      {
+        "id": "8056597233958",
+        "name": "OS7059",
+        "color": "Crystal",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7059-CRY+(2024).jpg"
+      },
+      {
+        "id": "8053672909258",
+        "name": "OS7059",
+        "color": "Grey",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7059-GRY+(2024).jpg"
+      },
+      {
+        "id": "0888392486523",
+        "name": "OS7059",
+        "color": "Navy",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7059-NAV+(2024).jpg"
+      },
+      {
+        "id": "8056597233958",
+        "name": "OS7060",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7060-BLK+(2024).jpg"
+      },
+      {
+        "id": "8053672909258",
+        "name": "OS7060",
+        "color": "Tortoise",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7060-TOR+(2024).jpg"
+      },
+      {
+        "id": "0888392486523",
+        "name": "OS7062",
+        "color": "Navy",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7062-NAV+(2024).jpg"
+      },
+      {
+        "id": "8056597233958",
+        "name": "OS7080",
+        "color": "Navy",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7080-NAV+(2024).jpg"
+      },
+      {
+        "id": "8053672909258",
+        "name": "OS7081",
+        "color": "Black Fade",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7081-BLF+(2024).jpg"
+      },
+      {
+        "id": "0888392486523",
+        "name": "OS7081",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7081-BLK+(2024).jpg"
+      },
+      {
+        "id": "8056597233958",
+        "name": "OS7082",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7082-BLK+(2024).jpg"
+      },
+      {
+        "id": "8053672909258",
+        "name": "OS7083",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7083-BLK+(2024).jpg"
+      },
+      {
+        "id": "0888392486523",
+        "name": "OS7083",
+        "color": "Pink",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7083-PNK+(2024).jpg"
+      },
+      {
+        "id": "8056597233958",
+        "name": "OS7083",
+        "color": "Tortoise",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7083-TOR+(2024).jpg"
+      },
+      {
+        "id": "8053672909258",
+        "name": "OS7084",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7084-BLK+(2024).jpg"
+      },
+      {
+        "id": "0888392486523",
+        "name": "OS7084",
+        "color": "Gold",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7084-GLD+(2024).jpg"
+      },
+      {
+        "id": "8056597233958",
+        "name": "OS7089",
+        "color": "Black",
+        "type": "ZYL",
+        "measurements": "45-17-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS7089-BLK+(2024).jpg"
+      },
+      {
+        "id": "8053672909258",
+        "name": "OS8008",
+        "color": "Gunmetal",
+        "type": "METAL",
+        "measurements": "44-19-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS8008-GUN+(2024).jpg"
+      },
+      {
+        "id": "0888392486523",
+        "name": "OS8010",
+        "color": "Blue",
+        "type": "METAL",
+        "measurements": "45-19-125",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS8010-BLU+(2024).jpg"
+      },
+      {
+        "id": "8056597233958",
+        "name": "OS8010",
+        "color": "Purple",
+        "type": "METAL",
+        "measurements": "45-19-125",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS8010-PRP+(2024).jpg"
+      },
+      {
+        "id": "8053672909258",
+        "name": "OS8018",
+        "color": "Pink",
+        "type": "ZYL",
+        "measurements": "45-16-130",
+        "image": "https://evfportal.s3.amazonaws.com/FrameImages/OSELF2024/OS8018-PNK+(2024).jpg"
+      }
+];
